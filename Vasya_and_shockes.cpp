082b55203cecab3aa5e1f16b892c1fdc23e7e51b
Muhsina Tasnim <105277856+Bookflea00@users.noreply.github.com>
2023-01-18 00:02:31 +0600

    Add files via upload
    CodeForces Solution

---
+++ Vasya_and_shockes.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n,m;
+    cin>>n>>m;
+    int ans=0;
+    
+    for(int i=1;i*m<=n;i++){
+        
+      n++;
+        }
+
+     //
+    
+    
+    cout<<n<<endl;}
+
+    
