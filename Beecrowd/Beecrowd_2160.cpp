916311401ac4336df8198f7aab5efb98e079ed99
Muhsina Tasnim <105277856+Bookflea00@users.noreply.github.com>
2023-01-30 20:50:46 +0600

    Add files via upload

---
+++ Beecrowd/Beecrowd_2160.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s;
+    int l;
+    //cin>>s;
+    getline(cin,s);
+    l=s.length();
+    if(l<80)
+    {
+        cout<<"YES"<<endl;
+    }
+    else
+    {
+        cout<<"NO"<<endl;
+    }
+}
